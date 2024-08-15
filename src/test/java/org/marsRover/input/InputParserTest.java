94fe61bdefe98de3e36ce4434e1b26bf8a9469a5
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-08-15 15:30:13 +0100

    Test: added test for a multiple input parser

--- src/test/java/org/marsRover/input/InputParserTest.java
+++ src/test/java/org/marsRover/input/InputParserTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class InputParserTest {
 
     @Test
-    void testParser_singleinput() {
+    void testParser_singleInputMixedCases() {
         //Arrange
         InputParser testParser = new InputParser();
         ArrayList<Instruction> expectedL = new ArrayList<>(List.of(Instruction.L));
@@ -19,13 +19,40 @@ class InputParserTest {
         ArrayList<Instruction> expectedM = new ArrayList<>(List.of(Instruction.M));
         //Act
         ArrayList<Instruction> testL = testParser.parser("L");
+        ArrayList<Instruction> testLLower = testParser.parser("l");
         ArrayList<Instruction> testR = testParser.parser("R");
+        ArrayList<Instruction> testRLower = testParser.parser("r");
         ArrayList<Instruction> testM = testParser.parser("M");
+        ArrayList<Instruction> testMLower = testParser.parser("m");
         // Assert
         assertAll(
                 () -> assertEquals(expectedL, testL),
+                () -> assertEquals(expectedL, testLLower),
                 () -> assertEquals(expectedR, testR),
-                () -> assertEquals(expectedM, testM)
+                () -> assertEquals(expectedR, testRLower),
+                () -> assertEquals(expectedM, testM),
+                () -> assertEquals(expectedM, testMLower)
+        );
+
+    }
+    @Test
+    void testParser_MultiInputMixedCases() {
+        //Arrange
+        InputParser testParser = new InputParser();
+        ArrayList<Instruction> expected1 = new ArrayList<>(List.of(Instruction.L, Instruction.L, Instruction.L));
+        ArrayList<Instruction> expected2 = new ArrayList<>(List.of(Instruction.L,Instruction.R, Instruction.R, Instruction.M));
+        ArrayList<Instruction> expected3 = new ArrayList<>(List.of(Instruction.M, Instruction.R, Instruction.L, Instruction.L, Instruction.R));
+        //Act
+        ArrayList<Instruction> test1 = testParser.parser("LLL");
+        ArrayList<Instruction> test2 = testParser.parser("LrRM");
+        ArrayList<Instruction> test3 = testParser.parser("MRlLr");
+
+        // Assert
+        assertAll(
+
+                () -> assertEquals(expected1, test1),
+                () -> assertEquals(expected2, test2),
+                () -> assertEquals(expected3, test3)
         );
 
     }
