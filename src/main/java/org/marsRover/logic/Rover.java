2d7a2b714e0ee820516ba7e8ac0b359b75d7d21c
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-08-15 17:07:53 +0100

    Test: added test for rotateL method

--- src/main/java/org/marsRover/logic/Rover.java
+++ src/main/java/org/marsRover/logic/Rover.java
@@ -8,38 +8,78 @@ public class Rover {
     private Position currentPosition;
 
     //TODO: Consider refactoring this because it is some messy code
-    public void move(Position startPosition, Instruction inst) {
-        if (inst == Instruction.L) {
-            if (startPosition.getFacing() == Direction.N) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
-            } else if (startPosition.getFacing() == Direction.W) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
-            } else if (startPosition.getFacing() == Direction.S) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
-            } else if (startPosition.getFacing() == Direction.E) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
-            }
-        } else if (inst == Instruction.R) {
-            if (startPosition.getFacing() == Direction.N) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
-            } else if (startPosition.getFacing() == Direction.E) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
-            } else if (startPosition.getFacing() == Direction.S) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
-            } else if (startPosition.getFacing() == Direction.W) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
-            }
-
-        } else if (inst == Instruction.M) {
-            if (startPosition.getFacing() == Direction.N) {
-                currentPosition = new Position(currentPosition.getX() + 1, currentPosition.getY(), currentPosition.getFacing());
-            } else if (startPosition.getFacing() == Direction.E) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY() + 1, currentPosition.getFacing());
-            } else if (startPosition.getFacing() == Direction.S) {
-                currentPosition = new Position(currentPosition.getX() - 1, currentPosition.getY(), currentPosition.getFacing());
-            } else if (startPosition.getFacing() == Direction.W) {
-                currentPosition = new Position(currentPosition.getX(), currentPosition.getY() - 1, currentPosition.getFacing());
-            }
+    //How are we going to pass this data?
+    // currently instructions are an array,
+    // we will need to iterate to get individual Instructions
+    // Would using a for loop with if-statements calling individual methods be better?
+    //
+//    public void move(Position startPosition, Instruction inst) {
+//        if (inst == Instruction.L) {
+//            if (startPosition.getFacing() == Direction.N) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
+//            } else if (startPosition.getFacing() == Direction.W) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
+//            } else if (startPosition.getFacing() == Direction.S) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
+//            } else if (startPosition.getFacing() == Direction.E) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
+//            }
+//        } else if (inst == Instruction.R) {
+//            if (startPosition.getFacing() == Direction.N) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
+//            } else if (startPosition.getFacing() == Direction.E) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
+//            } else if (startPosition.getFacing() == Direction.S) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
+//            } else if (startPosition.getFacing() == Direction.W) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
+//            }
+//
+//        } else if (inst == Instruction.M) {
+//            if (startPosition.getFacing() == Direction.N) {
+//                currentPosition = new Position(currentPosition.getX() + 1, currentPosition.getY(), currentPosition.getFacing());
+//            } else if (startPosition.getFacing() == Direction.E) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY() + 1, currentPosition.getFacing());
+//            } else if (startPosition.getFacing() == Direction.S) {
+//                currentPosition = new Position(currentPosition.getX() - 1, currentPosition.getY(), currentPosition.getFacing());
+//            } else if (startPosition.getFacing() == Direction.W) {
+//                currentPosition = new Position(currentPosition.getX(), currentPosition.getY() - 1, currentPosition.getFacing());
+//            }
+//        }
+//    }
+    public Position rotateL(Position startPosition) {
+        Position pos = startPosition;
+        if (startPosition.getFacing() == Direction.N) {
+            pos.setFacing(Direction.W);
+        } else if (startPosition.getFacing() == Direction.W) {
+            pos.setFacing(Direction.S);
+        } else if (startPosition.getFacing() == Direction.S) {
+            pos.setFacing(Direction.E);
+        } else if (startPosition.getFacing() == Direction.E) {
+            pos.setFacing(Direction.N);
+        }
+        return pos;
+    }
+    public void rotateR(Position startPosition) {
+        if (startPosition.getFacing() == Direction.N) {
+            currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
+        } else if (startPosition.getFacing() == Direction.E) {
+            currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
+        } else if (startPosition.getFacing() == Direction.S) {
+            currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
+        } else if (startPosition.getFacing() == Direction.W) {
+            currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
+        }
+    }
+    public void moveForwards(Position startPosition) {
+        if (startPosition.getFacing() == Direction.N) {
+            currentPosition = new Position(currentPosition.getX() + 1, currentPosition.getY(), currentPosition.getFacing());
+        } else if (startPosition.getFacing() == Direction.E) {
+            currentPosition = new Position(currentPosition.getX(), currentPosition.getY() + 1, currentPosition.getFacing());
+        } else if (startPosition.getFacing() == Direction.S) {
+            currentPosition = new Position(currentPosition.getX() - 1, currentPosition.getY(), currentPosition.getFacing());
+        } else if (startPosition.getFacing() == Direction.W) {
+            currentPosition = new Position(currentPosition.getX(), currentPosition.getY() - 1, currentPosition.getFacing());
         }
     }
 }
--- src/test/java/org/marsRover/logic/RoverTest.java
+++ src/test/java/org/marsRover/logic/RoverTest.java
@@ -1,12 +1,34 @@
 package org.marsRover.logic;
 
 import org.junit.jupiter.api.Test;
+import org.marsRover.input.Direction;
+import org.marsRover.input.Position;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoverTest {
 
+//    @Test
+//    void move() {
+//    }
+
     @Test
-    void move() {
+    void testRotateL() {
+        Rover curiosity = new Rover();
+        Position testStart = new Position(5,5, Direction.N);
+
+        Position result1 = curiosity.rotateL(testStart);
+        String r1 = result1.getFacing().toString();
+
+        String expected1 = Direction.W.name();
+        assertEquals(expected1,result1);
+    }
+
+    @Test
+    void rotateR() {
+    }
+
+    @Test
+    void moveForwards() {
     }
 }
