affc04ff7842206000cb1d237490a441b6cd6a33
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-08-15 16:22:00 +0100

    Feat: implimented move methodology

--- src/main/java/org/marsRover/logic/Rover.java
+++ src/main/java/org/marsRover/logic/Rover.java
@@ -1,7 +1,45 @@
 package org.marsRover.logic;
 
+import org.marsRover.input.Direction;
+import org.marsRover.input.Instruction;
 import org.marsRover.input.Position;
 
 public class Rover {
     private Position currentPosition;
+
+    //TODO: Consider refactoring this because it is some messy code
+    public void move(Position startPosition, Instruction inst) {
+        if (inst == Instruction.L) {
+            if (startPosition.getFacing() == Direction.N) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
+            } else if (startPosition.getFacing() == Direction.W) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
+            } else if (startPosition.getFacing() == Direction.S) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
+            } else if (startPosition.getFacing() == Direction.E) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
+            }
+        } else if (inst == Instruction.R) {
+            if (startPosition.getFacing() == Direction.N) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.E);
+            } else if (startPosition.getFacing() == Direction.E) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.S);
+            } else if (startPosition.getFacing() == Direction.S) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.W);
+            } else if (startPosition.getFacing() == Direction.W) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY(), Direction.N);
+            }
+
+        } else if (inst == Instruction.M) {
+            if (startPosition.getFacing() == Direction.N) {
+                currentPosition = new Position(currentPosition.getX() + 1, currentPosition.getY(), currentPosition.getFacing());
+            } else if (startPosition.getFacing() == Direction.E) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY() + 1, currentPosition.getFacing());
+            } else if (startPosition.getFacing() == Direction.S) {
+                currentPosition = new Position(currentPosition.getX() - 1, currentPosition.getY(), currentPosition.getFacing());
+            } else if (startPosition.getFacing() == Direction.W) {
+                currentPosition = new Position(currentPosition.getX(), currentPosition.getY() - 1, currentPosition.getFacing());
+            }
+        }
+    }
 }
