93a6c35b516b838940fc013b831816e7df8bf9a8
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-08-16 10:19:26 +0100

    Refactored: refactored parser

--- src/main/java/org/marsRover/input/InputParser.java
+++ src/main/java/org/marsRover/input/InputParser.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 
 public class InputParser {
     public ArrayList<Instruction> parser(String input) {
-        char[] inputArray = input.toCharArray();
+        char[] inputArray = input.toUpperCase().toCharArray();
         ArrayList<Instruction> parsed = new ArrayList<>();
-// TODO: Refactor this to switch-case, Does this need to parse int values for Plateau and Rover creation?
+// TODO: Does this need to parse int values for Plateau and Rover creation?
 
-            for (int i = 0; i < inputArray.length; i++) {
-                if ((inputArray[i] == 'l') || (inputArray[i] == 'L')) {
+        for (char c : inputArray) {
+            switch (c) {
+                case 'L':
                     parsed.add(Instruction.L);
-                } else if ((inputArray[i] == 'r') || (inputArray[i] == 'R')) {
+                    break;
+                case 'R':
                     parsed.add(Instruction.R);
-                } else if ((inputArray[i] == 'm') || (inputArray[i] == 'M')) {
+                    break;
+                case 'M':
                     parsed.add(Instruction.M);
-                } else {
-                    //Works for now to not accept invalid inputs, however this will end the program not ideal
-                    throw new RuntimeException("Your Input contained invalid characters, please try again");
-                }
+                    break;
 
             }
-
+        }
         return parsed;
     }
 
